54969ab239fa517196157cf79ed30a8395e06be7
Isiah Smith <isiahthomassmith@gmail.com>
2021-10-29 16:26:44 -0500

    GET made on server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const { Router } = require('express');
+const pool = require('./modules/pool');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -9,7 +11,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+Router.get('/', (req, res) => {
+    pool.query('SELECT * FROM "feedback";')
+    .then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('Error in server GET', error);
+        res.sendStatus(500);
+    });
+});
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
